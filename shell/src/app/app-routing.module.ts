4347e5a48c03b8f14f7ee8334a11d175901ddd98
Amaldev <amaldev.psn@gmail.com>
2024-11-07 23:06:15 +0530

    c

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', loadComponent: () => import('profile/ProfileComponent').then(m => m.ProfileComponent) },
+  { path: 'dashboard', loadComponent: () => import('dashboard/DashboardComponent').then(m => m.DashboardComponent) },
+  { path: '', redirectTo: '/profile', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
