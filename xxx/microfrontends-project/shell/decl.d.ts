1a49824fe5b70ced2a147ac927e1c476c2359f44
Amaldev <amaldev.psn@gmail.com>
2024-11-07 22:45:09 +0530

    rebuilding

--- xxx/microfrontends-project/shell/decl.d.ts
+++
@@ -1,8 +0,0 @@
-
-declare module 'profile/ProfileComponent' {
-  export const ProfileComponent: any;
-}
-
-declare module 'dashboard/DashboardComponent' {
-  export const DashboardComponent: any;
-}
